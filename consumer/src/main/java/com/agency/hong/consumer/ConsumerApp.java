4bed02f2738231ccc852d170f7e84929ad4f5f0a
hong <15084924601@163.com>
2018-12-20 18:11:21 +0800

    first step  Hystrix

--- consumer/src/main/java/com/agency/hong/consumer/ConsumerApp.java
+++ consumer/src/main/java/com/agency/hong/consumer/ConsumerApp.java
@@ -2,6 +2,7 @@ package com.agency.hong.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCircuitBreaker
 public class ConsumerApp 
 {
     public static void main( String[] args )
--- consumer/src/main/java/com/agency/hong/consumer/service/TestService.java
+++ consumer/src/main/java/com/agency/hong/consumer/service/TestService.java
@@ -1,14 +1,23 @@
 package com.agency.hong.consumer.service;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("client")
+@FeignClient(name = "client", fallback = TestService.GreetingClientFallback.class)
 public interface TestService {
-	
-	@RequestMapping(value="/dc",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/dc", method = RequestMethod.GET)
 	String consumer();
 
+	@Component
+	public static class GreetingClientFallback implements TestService {
+
+		@Override
+		public String consumer() {
+			return "client broken";
+		}
+
+	}
 }
